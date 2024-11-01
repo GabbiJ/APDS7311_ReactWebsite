d6918bde27cf74126e34f689b73b72c4e6f62055
ST10034968 <ST10034968@vcconnect.edu.za>
2024-11-01 13:53:10 +0200

    made adjustments to employee home page, navigation for it works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Register from './components/Register';
 import LoginEmp from './components/LoginEmp';
 import EmployeeHome from './components/HomeEmp';
 import GreetingPage from './components/GreetingPage';
+import ViewPayments from './components/ViewPayments';
+import ViewVerifiedPayments from './components/ViewVerifiedPayments';
 import './App.css'
 
 // Function to check if the user is authenticated
@@ -17,6 +19,8 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/employee-login" element={<LoginEmp />} />
                 <Route path="/greeting-page" element={<GreetingPage />} />
+                <Route path="/view-payments" element={<ViewPayments />} />
+                <Route path="/view-verified-payments" element={<ViewVerifiedPayments />} />
                 <Route
                     path="/employee-home"
                     element={isAuthenticated() ? <EmployeeHome /> : <Navigate to="/employee-login" />}
---
+++ frontend/src/components/ViewVerifiedPayments.js
@@ -0,0 +1,16 @@
+//viewing all verified payments
+
+import React, { useEffect, useState } from 'react';
+
+function ViewVerifiedPayments() 
+{
+
+    return (
+        <div>
+            <h2>Verified Payments</h2>
+            
+        </div>
+    );
+}
+
+export default ViewVerifiedPayments;
